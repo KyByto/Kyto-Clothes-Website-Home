369486fd0090b247373bf0de104225998040ce23
Kybyto <136782115+KyByto@users.noreply.github.com>
2024-02-12 14:38:46 +0100

    changing the source image

--- src/Slider Components/SwiperSlide.jsx
+++ src/Slider Components/SwiperSlide.jsx
@@ -4,7 +4,7 @@ export default function ImageSlide({type,number,width} ) {
     return (
         <div className="swiper-slide">
             
-            <img src={`/public/images/${type}/${number}/${width}.jpg`}/>
+            <img src={`../images/${type}/${number}/${width}.jpg`}/>
             </div>
     )
 }
